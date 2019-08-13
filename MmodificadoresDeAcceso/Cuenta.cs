12028c5cbcf35d718f191371daaaec78140b009f
Alexia Ruiz <alexiaruiz-@hotmail.com>
2019-08-13 07:49:53 -0700

    Otra clase

---
+++ MmodificadoresDeAcceso/Cuenta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MmodificadoresDeAcceso
+{
+    class Cuenta
+    {
+        public string Numero de cuenta { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Saldo { get; set; }
+
+        public string  { get; set; }
+
+        private string _curp;
+        public string Curp {
+            get {
+                return _curp;
+            }
+            set {
+                if (value.Length == 18)
+                {
+                    _curp = value;
+                }
+                else
+                {
+                    throw new Exception("No es de 18 caracteres");
+                }
+            }
+        }
+
+    }
+}
